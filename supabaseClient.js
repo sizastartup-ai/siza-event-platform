ebbf21f46890094aff46244e39cbc9912310d91a
Siza Startup <sizastartup@gmail.com>
2026-03-13 11:53:11 +0300

    Fix syntax error in supabaseClient

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,8 +1,8 @@
 const { createClient } = require('@supabase/supabase-js');
 
-// Using process.env to fetch the variables from the Railway dashboard
-const supabaseUrl = process.env.https://lzryobzkamunzemtsvtc.supabase.co;
-const supabaseKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6cnlvYnprYW11bnplbXRzdnRjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI3OTE5NjIsImV4cCI6MjA4ODM2Nzk2Mn0.6mSv3WqXdz7k5D94sAxqd2Vg1O9kQrGLUWsvL - bSh8I;
+// This tells the app to look for the variable name, NOT the actual URL
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
